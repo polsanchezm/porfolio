1cec147c2b4fc22a12c8e61994522134542f0749
Pol Sanchez <polsamo2003@gmail.com>
2025-11-28 14:55:51 +0100

    Layout actualizado

--- src/components/layout/MainLayoutComponent.vue
+++ src/components/layout/MainLayoutComponent.vue
@@ -1,37 +1,29 @@
 <script setup lang="ts">
 import Header from '@/components/layout/HeaderComponent.vue'
 import Footer from '@/components/layout/FooterComponent.vue'
-import FaceCard from '@/components/layout/FaceCardComponent.vue'
 import ProjectGrid from '@/components/projects/ProjectGridComponent.vue'
 import AboutMe from '@/components/pages/AboutMeComponent.vue'
 import Navbar from '@/components/layout/NavbarComponent.vue'
 import ContactMe from '@/components/pages/ContactMeComponent.vue'
 import TechnologiesComponent from '@/components/technologies/TechnologyListComponent.vue'
-import MusicComponent from '@/components/music/MusicComponent.vue'
+import StackComponent from '@/components/technologies/StackComponent.vue'
+import ChristmasSnowflakes from '@/components/ui/ChristmasSnowflakes.vue'
 </script>
 
 <template>
   <main>
+    <ChristmasSnowflakes />
     <Navbar />
-    <div class="flex min-h-screen">
-      <aside
-        class="hidden min-[1400px]:flex lg:sticky lg:top-0 lg:w-1/4 lg:h-screen lg:flex-col lg:justify-center ml-24"
-      >
-        <FaceCard />
-      </aside>
-
+    <div class="justify-center items-center min-h-screen">
       <section class="p-6">
         <Header />
-        <div class="min-[1400px]:hidden lg:flex">
-          <FaceCard />
-        </div>
         <AboutMe />
         <ProjectGrid />
+        <StackComponent />
         <TechnologiesComponent />
         <ContactMe />
         <Footer />
       </section>
     </div>
-    <MusicComponent />
   </main>
 </template>
